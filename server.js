488312ed4555eb14d770f41fa2f1794d2b9aa6dc
PacemakerX <sparsh.officialwork@gmail.com>
2025-02-20 22:16:15 +0530

    making use of express p.s. sorry for index.js

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3030;
 
-const validHTML = `<!DOCTYPE html>
-<html>
-    <head>
-        <title>My Server</title>
-    </head>
-    <body>
-        <h1>Welcome to My Server, How are you doing guys?</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </body>
-</html>`;
-const server = http.createServer((req, res) => {
-  console.log("Got a new Request");
-  let response = "";
-
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      response = "hello world";
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(validHTML);
-    } else if (req.url === "/home") response = "Welcome to Home page";
-    else if (req.url === "/about") response = "Welcome to about page";
-    else response = "Page not found";
-  } else if (req.method === "POST") {
-    if (req.url === "/submit") response = "Data submitted successfully";
-    else response = "Page not found";
-  } else {
-    response = "Method not allowed";
-  }
-//   res.write(response);
-  res.end();
+app.get("/", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+        </html>
+    `);
 });
-// but this is very cluttered way to write code and not maintainable so we use express.js
-server.listen(3030, () => {
-  console.log("server is listening on port 3000");
+
+app.get("/home", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Home Page</title>
+        </head>
+        <body>
+            <h1>Welcome to Home Page</h1>
+        </body>
+        </html>
+    `);
+});
+
+app.get("/about", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>About Us</title>
+        </head>
+        <body>
+            <h1>About Us</h1>
+        </body>
+        </html>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
