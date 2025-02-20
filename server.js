ff6192950b0e92d52e2c7e8b4c5b7a40abc460fc
PacemakerX <sparsh.officialwork@gmail.com>
2025-02-20 21:36:04 +0530

    ya boi,first server

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("Got a new Request");
-  res.write("hello");
+  let response = "";
+  if (req.url === "/") response = "hello world";
+  else if (req.url === "/home") response = "Welcome to Home page";
+  else if (req.url === "/about") response = "Welcome to about page";
+  else response = "Page not found";
+  res.write(response);
   res.end();
-}); // a server is created, now we have  to make it listen to PORTS, which room are we talking about in an room,
-
+});
 server.listen(3030, () => {
   console.log("server is listening on port 3000");
 });
