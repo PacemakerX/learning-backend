2b084f9ba920c478b0cc070e6e0ae2d65dfd8781
PacemakerX <sparsh.officialwork@gmail.com>
2025-02-21 01:35:00 +0530

    adds a authentication function

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ function middleware(req, res, next) {
   res.customData = "This is custom data";
   next();
 }
+
+function authenticate(req, res, next) {
+    res.statusCode=401;
+    res.send("You are not authenticated");
+    next();
+}
+
 app.use("/", middleware, mainRoutes);
 app.use("/posts", middleware, postRoutes);
-app.use("/users", middleware, userRoutes);
+app.use("/users", authenticate, userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
