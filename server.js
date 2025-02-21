25c7421da878c8ff9a8c608d20dfd2ea64d433a9
PacemakerX <sparsh.officialwork@gmail.com>
2025-02-21 23:34:33 +0530

    adds routes for post,integrate DB

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const port = 3030;
 const userRoutes = require("./routes/userRoutes");
@@ -8,13 +8,13 @@ const postRoutes = require("./routes/postRoutes");
 const middleware = require("./middlewares/basicMiddle");
 const auth = require("./middlewares/auth");
 const connectToDatabase = require("./db");
-connectToDatabase(); 
+connectToDatabase();
+app.use(cors());
+app.use(express.json());
 
+app.use("/api/post",middleware, postRoutes);
 app.use("/", middleware, mainRoutes);
-app.use("/posts", middleware, postRoutes);
-app.use("/users", auth, userRoutes);
-// we will use cors
-// app.use(cors());
+// app.use("/users", auth, userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
