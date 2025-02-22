b4d530872d0e82b87b20381a1ba4a16414fe8d1c
PacemakerX <sparsh.officialwork@gmail.com>
2025-02-22 19:16:25 +0530

    updates model adds email and password validator

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@([^\s@]+\.[^\s@]+)$/;
+  const allowedDomains = [
+    "gmail.com",
+    "yahoo.com",
+    "outlook.com",
+    "protonmail.com",
+  ];
+
+  const match = email.match(emailRegex);
+  return match ? allowedDomains.includes(match[1]) : false;
+};
+
+const validatePassword = (password) => {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
+};
 
 const userSchema = new mongoose.Schema(
   {
@@ -8,12 +27,11 @@ const userSchema = new mongoose.Schema(
       required: [true, "Username is required"],
       trim: true,
       maxLengh: 50,
-    //   unique: true,
     },
     email: {
       type: String,
       required: [true, "Email is required"],
-    //   unique: true,
+      unique: true,
     },
     password: {
       type: String,
@@ -28,15 +46,28 @@ const userSchema = new mongoose.Schema(
 
 userSchema.statics.register = async function (name, email, password) {
   try {
-    // Hash the password using bcrypt
+    // Validate email format
+    if (!validateEmail(email)) {
+      throw new Error("Invalid email format");
+    }
+
+    // Validate password strength
+    if (!validatePassword(password)) {
+      throw new Error(
+        "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character."
+      );
+    }
+    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
+    // Create user
     const user = new this({
       name,
       email,
-      password: hashedPassword, // Store the hashed password
+      password: hashedPassword,
     });
+
     const newUser = await user.save();
     return newUser;
   } catch (error) {
