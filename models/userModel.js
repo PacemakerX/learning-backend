9b0b501516f53017a0d15e7c8f8ab59afa6c5b9c
PacemakerX <sparsh.officialwork@gmail.com>
2025-02-22 19:26:26 +0530

    updates validation, now uses validator lib

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-
-const validateEmail = (email) => {
-  const emailRegex = /^[^\s@]+@([^\s@]+\.[^\s@]+)$/;
-  const allowedDomains = [
-    "gmail.com",
-    "yahoo.com",
-    "outlook.com",
-    "protonmail.com",
-  ];
-
-  const match = email.match(emailRegex);
-  return match ? allowedDomains.includes(match[1]) : false;
-};
-
-const validatePassword = (password) => {
-  const passwordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  return passwordRegex.test(password);
-};
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -47,16 +29,20 @@ const userSchema = new mongoose.Schema(
 userSchema.statics.register = async function (name, email, password) {
   try {
     // Validate email format
-    if (!validateEmail(email)) {
-      throw new Error("Invalid email format");
-    }
-
-    // Validate password strength
-    if (!validatePassword(password)) {
-      throw new Error(
-        "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character."
-      );
-    }
+    if (!validator.isEmail(email)) {
+        throw new Error("Invalid email format.");
+      }
+  
+      // Validate password strength
+      if (!validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })) {
+        throw new Error("Weak password. It must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character.");
+      }
     // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
